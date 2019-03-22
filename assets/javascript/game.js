1f72716322bea89896b3117c463a941ae7549a0b
gabbistein <gabbistein@outlook.com>
2019-03-22 02:02:15 -0700

    upgrades

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,76 @@
-var myWins = 0;
 
-var myLosses = 0;
+$(document).ready(function() {
 
-var numOne = document.getElementById("crystalOne");
-    numOne.setAttribute("value", "4");
+    // Each crystal is randomly going to be worth 1-12.
+    var numOne = Math.floor(Math.random()* 13);
+    var numTwo = Math.floor(Math.random()* 13);
+    var numThree = Math.floor(Math.random()* 13);
+    var numFour = Math.floor(Math.random()* 13);
+    
+    // The total of your guesses each turn.
+    var myScore = 0;
+    
+    // To generate the random score you're trying to reach.
+    var minNumber = 19;
+    var maxNumber = 120;
+    var randomScore = randomFromRange(minNumber, maxNumber);
 
-var numTwo = document.getElementById("crystalTwo");
-    numTwo.setAttribute("value", "7");
+        function randomFromRange(min,max) {
+            return Math.floor(Math.random()*(max-min+1)+min);
+        }
+        console.log(randomScore);
+        $("#scoreToHit").text(randomScore);
 
-var numThree = document.getElementById("crystalThree");
-    numThree.setAttribute("value", "10");
+    // These hold your win and loss counts.
+    var playerWins = 0;
+    var playerLosses = 0;
 
-var numFour = document.getElementById("crystalFour");
-    numFour.setAttribute("value", "5");
+    // --GAME LOGIC BELOW--
+
+    
 
 
-var myScore = document.getElementById("playerScore");
-    myScore.innerHTML = 0;
-var goalScore = document.getElementById("scoreToHit");
-    goalScore.innerHTML = getRandomValue(19, 101);
 
-function addToScore(val) {
-  var numberToAdd = parseInt(val);
-  var currentScore = parseInt(myScore.innerHTML);
-  myScore.innerHTML = numberToAdd + currentScore;
-}
 
-numOne.addEventListener("click", function() {
-  addToScore(this.value);
-  checkPlayerScore();
+
+
+
+    // function addToScore(val) {
+    //     var numberToAdd = parseInt(val);
+    //     var currentScore = parseInt(myScore.innerHTML);
+    //     myScore.innerHTML = numberToAdd + currentScore;
+    // }
+
+    // numOne.addEventListener("click", function() {
+    //     addToScore(this.value);
+    //     checkPlayerScore();
+    // });
+
+    // numTwo.addEventListener("click", function() {
+    //     addToScore(this.value);
+    //     checkPlayerScore();
+    // });
+
+    // numThree.addEventListener("click", function() {
+    //     addToScore(this.value);
+    //     checkPlayerScore();
+    // });
+
+    // numFour.addEventListener("click", function() {
+    //     addToScore(this.value);
+    //     checkPlayerScore();
+    // });
+
+    // function checkPlayerScore() {
+    //     var playerInt = parseInt(myScore.innerHTML);
+    //     var targetInt = parseInt(randomScore.innerHTML);
+    //     if (playerInt === targetInt) {
+    //         // You're a winner! How to get this to add up?
+    //     } else if (playerInt > targetInt) {
+    //         // You lose the game. Restart?
+    //     } 
+    // }
+
+    // // I can't quite figure out how to set the win condition here.
+
 });
-
-numTwo.addEventListener("click", function() {
-  addToScore(this.value);
-  checkPlayerScore();
-});
-
-numThree.addEventListener("click", function() {
-  addToScore(this.value);
-  checkPlayerScore();
-});
-
-numFour.addEventListener("click", function() {
-  addToScore(this.value);
-  checkPlayerScore();
-});
-
-
-function getRandomValue (a, b){
-  return Math.floor((Math.random() * a) + b);
-}
-
-function checkPlayerScore() {
-  var playerInt = parseInt(myScore.innerHTML);
-  var targetInt = parseInt(goalScore.innerHTML);
-  if (playerInt === targetInt) {
-    // You're a winner! How to get this to add up?
-  } else if (playerInt > targetInt) {
-    // You lose the game. Restart?
-  } 
-}
-
-// I can't quite figure out how to set the win condition here.
